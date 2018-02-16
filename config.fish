ab2b92a534adcedb62a373f354c16e7c48562a5d
Josh Greenwood <joshua.t.greenwood@gmail.com>
2018-02-15 21:39:01 -0500

    adds fish config file

--- config.fish
+++ config.fish
@@ -1,7 +1,10 @@
 alias be="bundle exec"
 alias run_circle_failures="circle | grep 'spec/' | cut -f 1 -d ' ' | uniq | xargs bundle exec rspec"
+alias vim=nvim
 
 set PATH $HOME/.rbenv/shims $PATH
-alias e="emacsclient -t"
+function e
+  emacsclient -t
+end
 set EDITOR e
 set VISUAL e
