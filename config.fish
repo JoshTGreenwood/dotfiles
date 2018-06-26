3f61f2b2825267afbdc59505215155f18572241a
Josh Greenwood <joshua.t.greenwood@gmail.com>
2018-06-26 13:34:30 -0600

    adds a secrets file and random config for capybara webkit

--- config.fish
+++ config.fish
@@ -3,8 +3,15 @@ alias run_circle_failures="circle | grep 'spec/' | cut -f 1 -d ' ' | uniq | xarg
 alias vim=nvim
 
 set PATH $HOME/.rbenv/shims $PATH
+
+# this was necessary to install capybara-webkit
+set PATH "/usr/local/opt/qt@5.5/bin" $PATH
+
+# always use emacsclient
 function e
   emacsclient -t
 end
 set EDITOR e
 set VISUAL e
+
+. ~/.config/fish/secrets.fish
