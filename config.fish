e04350ad89dd26214de5e320272cbde884ba37b4
Josh Greenwood <joshua.t.greenwood@gmail.com>
2019-03-03 20:15:38 -0500

    kills some dead config, sets nvim as EDITOR

--- config.fish
+++ config.fish
@@ -1,17 +1,6 @@
 alias be="bundle exec"
-alias run_circle_failures="circle | grep 'spec/' | cut -f 1 -d ' ' | uniq | xargs bundle exec rspec"
-alias vim=nvim
 
-set PATH $HOME/.rbenv/shims $PATH
-
-# this was necessary to install capybara-webkit
-set PATH "/usr/local/opt/qt@5.5/bin" $PATH
-
-# always use emacsclient
-function e
-  emacsclient -t
-end
-set EDITOR e
-set VISUAL e
+set -x PATH $HOME/.rbenv/shims $PATH
+set -x EDITOR nvim
 
 . ~/.config/fish/secrets.fish
