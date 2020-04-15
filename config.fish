6795eee0599f34cd7be31d59033d6feed39d60a3
Josh Greenwood <joshua.t.greenwood@gmail.com>
2020-04-15 07:58:52 -0400

    disable spring system wide

--- config.fish
+++ config.fish
@@ -4,4 +4,8 @@ alias "weather=curl 'http://wttr.in/45227'"
 set -x PATH $HOME/.rbenv/shims $PATH
 set -x EDITOR vim
 
+# disable spring system wide
+# https://github.com/rails/rails/issues/26158
+set -x DISABLE_SPRING 1
+
 . ~/.config/fish/secrets.fish
