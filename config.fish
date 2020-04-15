467da483764e15138c7b97c68245312763e8e336
Josh Greenwood <joshua.t.greenwood@gmail.com>
2020-04-15 07:59:18 -0400

    adds nodeenv

--- config.fish
+++ config.fish
@@ -2,6 +2,7 @@ alias be="bundle exec"
 alias "weather=curl 'http://wttr.in/45227'"
 
 set -x PATH $HOME/.rbenv/shims $PATH
+status --is-interactive; and source (nodenv init -|psub)
 set -x EDITOR vim
 
 # disable spring system wide
