2183c9959ac90083baae12db5f94e592fe00e956
Josh Greenwood <joshua.t.greenwood@gmail.com>
2021-07-12 09:15:04 -0400

    swtich to asdf vs. rbenv and nodenv

--- config.fish
+++ config.fish
@@ -1,12 +1,13 @@
 alias be="bundle exec"
 alias "weather=curl 'http://wttr.in/45227'"
 
-set -x PATH $HOME/.rbenv/shims $PATH
-status --is-interactive; and source (nodenv init -|psub)
 set -x EDITOR vim
+
+source /usr/local/opt/asdf/asdf.fish
 
 # disable spring system wide
 # https://github.com/rails/rails/issues/26158
 set -x DISABLE_SPRING 1
 
 . ~/.config/fish/secrets.fish
+
