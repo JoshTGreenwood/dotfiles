bb5eb316c99e42afc0de15cb03330d2fd0ccf749
Josh Greenwood <joshua.t.greenwood@gmail.com>
2024-05-11 22:23:01 -0400

    add direnv to fish

--- config.fish
+++ config.fish
@@ -3,7 +3,8 @@ alias "weather=curl 'http://wttr.in/45227'"
 
 set -x EDITOR vim
 
-source /usr/local/opt/asdf/asdf.fish
+source /usr/local/opt/asdf/libexec/asdf.fish
+direnv hook fish | source
 
 # disable spring system wide
 # https://github.com/rails/rails/issues/26158
