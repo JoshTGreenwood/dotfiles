dd117dbb40137ff835ae8cca6c2032f98db8fcd2
Josh Greenwood <joshua.t.greenwood@gmail.com>
2026-03-05 14:59:12 -0500

    add starship prompt. cleanup.

--- config.fish
+++ config.fish
@@ -1,14 +1,11 @@
+. ~/.config/fish/secrets.fish
+
 alias be="bundle exec"
-alias "weather=curl 'http://wttr.in/45227'"
+alias weather="curl 'http://wttr.in/45227'"
 
 set -x EDITOR nvim
+set -x DISABLE_SPRING 1 # disable spring system wide https://github.com/rails/rails/issues/26158
 
 mise activate fish | source
 direnv hook fish | source
-
-# disable spring system wide
-# https://github.com/rails/rails/issues/26158
-set -x DISABLE_SPRING 1
-
-. ~/.config/fish/secrets.fish
-
+starship init fish | source
